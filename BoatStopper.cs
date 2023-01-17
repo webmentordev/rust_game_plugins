fc94ababb0ac148f476abd89871cec199af33493
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-01-17 18:15:41 +0500

    added plugin

---
+++ BoatStopper.cs
@@ -0,0 +1,25 @@
+﻿using Oxide.Plugins;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Plugins
+{
+    [Info("BoatStopper", "Ahmer", "1.0.0")]
+    [Description("Stop Row Boat, If noone is on it.")]
+    internal class BoatStopper: RustPlugin
+    {
+        object OnEntityDismounted(BaseMountable entity, BasePlayer player)
+        {
+            MotorRowboat myRowBoat = entity.GetParentEntity() as MotorRowboat;
+            if(!myRowBoat.AnyMounted() && !myRowBoat.HasDriver())
+            {
+                myRowBoat.EngineToggle(false);
+            }
+            return null;
+        }
+    }
+}
---
+++ Welcomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Plugins
+{
+    [Info("Welcomer", "Ahmer", "1.0.0")]
+    [Description("Welcome Player with message!")]
+    public class Welcomer : RustPlugin
+    {
+        void OnPlayerConnected(BasePlayer player)
+        {
+            player.ChatMessage("<size=16>Welcome to RustyOcean <color=#226de6>50x</color></size>.\n----------------------------\n* Server wipes evey Thursday at 6PM GMT\n* No Racism or Cheating.\n* No Teaming. Max 3 Players team is allowed.\n* Open backpack with /backpack\n* Upgrade while building with /bgrade 1-4");
+        }
+    }
+}
